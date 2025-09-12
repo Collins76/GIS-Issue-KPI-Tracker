98fdc3bd070127ba8a1d334393329726f01680c3
Collins Anyanwu <polosoft.collins@gmail.com>
2025-09-12 05:18:03 +0000

    import dynamic from 'next/dynamic';
    const DynamicAuthButton = dynamic((

--- src/components/page/app-header.tsx
+++ src/components/page/app-header.tsx
@@ -1,11 +1,16 @@
 import { Map } from 'lucide-react';
-import AuthButton from './auth-button';
+import dynamic from 'next/dynamic';
+
+const DynamicAuthButton = dynamic(() => import('./auth-button'), {
+  ssr: false,
+});
+
 
 const AppHeader = () => {
   return (
     <header className="text-white p-8 rounded-xl mb-8 text-center animate-fade-in shadow-lg bg-gradient-to-r from-primary to-secondary relative">
       <div className="absolute top-4 right-4">
-        <AuthButton />
+        <DynamicAuthButton />
       </div>
       <h1 className="text-4xl font-bold font-headline flex items-center justify-center gap-3">
         <Map className="w-10 h-10" />
